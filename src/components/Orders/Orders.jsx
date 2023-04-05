ac9214783e46595c8e712eb36c7a75ff0c8b1e8f
AhaD <abuahad35188@gmail.com>
2023-04-05 12:26:45 +0600

    order card complet

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cards from '../Cards/Cards';
 import { useLoaderData } from 'react-router-dom';
- 
+import ReviewItem from '../ReviewItem/ReviewItem';
+ import './orders.css'
+import { removeFromDb } from '../../../utilities/fakedb';
 
 const Orders = () => {
-      const cart = useLoaderData();
-      console.log(cart)
+      const saveCart = useLoaderData();
+      const [cart,setCart] = useState(saveCart);
+      const removeHandeler =(id)=>{
+          // console.log(id)
+          const remainingCard = cart.filter(p=>p.id !== id);
+          setCart(remainingCard);
+          removeFromDb(id)
+      }
+     //  console.log(cart)
      return (
           <div className='shop-container'>
-               <div className='Products-container'>
-               <h1>this is order side:{cart.length}</h1>
+               <div className='review-container'>
+                
+               {
+                    cart.map(product=><ReviewItem
+                    key={product.id}
+                    product={product}
+                    removeHandeler={removeHandeler}
+                    ></ReviewItem>)
+               }
                </div>
                <div className='cart-container'>
                     <Cards cart={cart}></Cards>
---
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ReviewItem.css';
+import {FaTrashAlt} from "react-icons/fa";
+const ReviewItem = ({product,removeHandeler}) => {
+     const {id,img,price,name, quantity} = product
+     return (
+          <div className='review-item'>
+               <img src={img} alt="" />
+               <div className="review-details">
+                    <p className='r-title'>{name}</p>
+                    <p>Price:$<span>{price}</span></p>
+                    <p>Quantity:<span>{quantity}</span></p>
+               </div>
+               <button className='d-btn' onClick={()=>removeHandeler(id)}>
+                    
+                     <FaTrashAlt className='d-icon'></FaTrashAlt>
+                    </button>
+          </div>
+     );
+};
+
+export default ReviewItem;
